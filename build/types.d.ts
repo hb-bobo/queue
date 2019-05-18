2e0916e581d2a66798fafc2904701b7d832ea184
hubo36 <hubo36@jd.com>
2019-05-18 16:39:26 +0800

    build:1.0.2

--- build/types.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Config {
-    limit: number;
-    autoStart?: boolean;
-    autoDoNext?: boolean;
-}
-declare type Done = () => void;
-export declare type QueueItem = (done: Done) => void;
-export declare type LineType = 'processingQueue' | 'processedQueue';
-export {};
